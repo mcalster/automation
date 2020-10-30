3264029645105f07426d24355f33b47ba7de620f
Kim Alster Glimberg <kagli@kims-mbp.lan>
2020-10-30 13:28:55 +0100

    Commit added automatically due to file changed
    Signed-off-by: Kim Alster Glimberg <kagli@kims-mbp.lan>

--- comitter.sh
+++ comitter.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
 UNTRACKED=$(git ls-files --others --exclude-standard)
-if [ ${UNTRACKED[@]:+${UNTRACKED[@]}} ] || [ -n "$( git status --porcelain)"]
+
+if [[ (${UNTRACKED[@]:+${UNTRACKED[@]}} || -n "$( git status --porcelain)")]]
 then
   echo "Repo has uncomitted changes"
 
   CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
-  if [[ $CURRENT_BRANCH = "master" ]]
+  if [ $CURRENT_BRANCH = "master" ]
   then
     echo "Currently on $CURRENT_BRANCH creating new branch"
-    git checkout -b "hassio/auto-created-$(date +'%d-%m-%y')"
+    CURRENT_BRANCH="hassio/auto-created-$(date +'%d-%m-%y')"
+    git checkout -b $CURRENT_BRANCH
   fi
 
-  if [[ -n $UNTRACKED ]]
+  if [ -n $UNTRACKED ]
   then
     echo "Repo has untracked files. Adding the following:"
     for each in "${UNTRACKED[@]}"
@@ -24,5 +26,5 @@ then
 
   echo "Committing uncomitted changes"
   git commit -sam "Commit added automatically due to file changed"
-  git push
+  git push --set-upstream origin $CURRENT_BRANCH
 fi
